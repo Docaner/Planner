8adf4f51649ee7472b45ded1dfc9f44ccec7f7df
Docaner <danil241202@gmail.com>
2023-06-21 18:03:38 +0300

    CanvasStart

--- Planner.Module.Diagram/ViewModels/StartWindowViewModel.cs
+++ Planner.Module.Diagram/ViewModels/StartWindowViewModel.cs
@@ -2,21 +2,37 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Planner.Module.Diagram.ViewModels
 {
     public class StartWindowViewModel : BindableBase
     {
         private String _header;
-
         public String Header
         {
             get => _header;
             set => SetProperty(ref _header, value);
         }
 
+        private int _windowHeight;
+        public int WindowHeight
+        {
+            get => _windowHeight;
+            set => SetProperty(ref _windowHeight, value);
+        }
+
+        private int _windowWidth;
+        public int WindowWidth
+        {
+            get => _windowWidth;
+            set => SetProperty(ref _windowWidth, value);
+        }
+
         public StartWindowViewModel()
         {
+            WindowHeight = 600;
+            WindowWidth = 800;
             Header = "Всем привет! Это ПЛАНИРОВЩИК!";
         }
     }
