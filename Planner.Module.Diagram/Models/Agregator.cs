3e995322774a7e362b875226929554973145d743
Vlad166666 <137280992+Vlad166666@users.noreply.github.com>
2023-06-21 19:36:27 +0300

    Add files via upload

---
+++ Planner.Module.Diagram/Models/Agregator.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Planner.Module.Diagram.Models
+{
+    public class Agregator:BindableBase
+    {
+        ObservableCollection<Plavki> plavkis;
+        private string _name;
+
+        private int _id;
+        public string Name
+        {
+            get => _name;set => SetProperty(ref _name, value);
+        }
+        public int Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+        public ObservableCollection<Plavki> Plavkis
+        {
+            get => plavkis;
+            set => SetProperty(ref plavkis, value);
+        }
+        public Agregator(int id)  
+        {
+            
+            Id = id;
+            Plavkis = new ObservableCollection<Plavki>();
+        }
+
+
+    }
+}
---
+++ Planner.Module.Diagram/Servises/MinutedToHoure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planner.Module.Diagram.Servises
+{
+   public class MinutedToHoure
+    {
+       public void  minutetohoure(int minuted,int min,int houre) {
+
+            houre=minuted/60;
+            min = minuted % 60;
+        
+        }
+    }
+}
