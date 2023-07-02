674a31d25302344c67d42b333ab3bc59bd0be977
ilyaslav <91390538+ilyaslav@users.noreply.github.com>
2023-07-03 00:01:48 +0300

    AgregatorsCollection(jsonReader)

---
+++ Planner.Module.Diagram/Models/AgregatorsCollection.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace Planner.Module.Diagram.Models
+{
+    public class AgregatorsCollection: ObservableCollection<Agregator>
+    {
+        public AgregatorsCollection(CanvasSettings settings) {
+            ObservableCollection<JsonAgregator> jsonAgregators = ReadJson();
+
+            foreach (JsonAgregator jsonAgregator in jsonAgregators){
+                Agregator agregator = new Agregator(jsonAgregator.name, new ObservableCollection<Melting>());
+                foreach (JsonMelting jsonMelting in jsonAgregator.meltings)
+                {
+                    agregator.Meltings.Add(new Melting(
+                        jsonMelting.heatNum, 
+                        jsonMelting.start,
+                        jsonMelting.end,
+                        "#ADFF2F",
+                        settings));
+                }
+                Add(agregator);
+            }
+        }
+
+        private ObservableCollection<JsonAgregator> ReadJson()
+        {
+            string json = File.ReadAllText("test.txt");
+            ObservableCollection<JsonAgregator> jsonAgregators = JsonConvert.DeserializeObject<ObservableCollection<JsonAgregator>>(json);
+            return jsonAgregators;
+        }
+
+        private class JsonAgregator
+        {
+            public string name { get; set; }
+            public ObservableCollection<JsonMelting> meltings { get; set; }
+        }
+        private class JsonMelting
+        {
+            public int heatNum { get; set; }
+            public DateTime start { get; set; }
+            public DateTime end { get; set; }
+        }
+    }
+}
--- Planner.Module.Diagram/ViewModels/StartWindowViewModel.cs
+++ Planner.Module.Diagram/ViewModels/StartWindowViewModel.cs
@@ -18,39 +18,7 @@ namespace Planner.Module.Diagram.ViewModels
 
             Filler = new CanvasFiller(settings);
 
-            ObservableCollection<Agregator> agregators = new ObservableCollection<Agregator>()
-            {
-                new Agregator
-                (
-                    "Agr1",
-                    new ObservableCollection<Melting>()
-                    {
-                        new Melting(1, DateTime.Now.AddHours(-1), DateTime.Now.AddHours(3),"#ADFF2F", settings),
-                        new Melting(2, DateTime.Now.AddHours(4), DateTime.Now.AddHours(6),"#8B0000", settings),
-                        new Melting(3, DateTime.Now.AddHours(7), DateTime.Now.AddHours(9),"#ADFF2F", settings)
-                    }
-                ),
-                new Agregator
-                (
-                    "Agr2",
-                    new ObservableCollection<Melting>()
-                    {
-                        new Melting(1, DateTime.Now.AddHours(0), DateTime.Now.AddHours(2), "#ADFF2F", settings),
-                        new Melting(2, DateTime.Now.AddHours(3), DateTime.Now.AddHours(5), "#1E90FF", settings),
-                        new Melting(3, DateTime.Now.AddHours(8), DateTime.Now.AddHours(10), "#ADFF2F", settings)
-                    }
-                ),
-                new Agregator
-                (
-                    "Agr3",
-                    new ObservableCollection<Melting>()
-                    {
-                        new Melting(1, DateTime.Now.AddHours(3), DateTime.Now.AddHours(6), "#ADFF2F", settings),
-                        new Melting(2, DateTime.Now.AddHours(7), DateTime.Now.AddHours(10), "#FFFF00", settings),
-                        new Melting(3, DateTime.Now.AddHours(11), DateTime.Now.AddHours(13), "#ADFF2F", settings)
-                    }
-                ),
-            };
+            ObservableCollection<Agregator> agregators = new AgregatorsCollection(settings);
 
             Filler.Agregators = agregators;
         }
